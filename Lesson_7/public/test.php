eac29bcbd815cb234c2063a46fbff893242541df
Maksim Shinkarenko <makc.pst@gmail.com>
2019-10-03 23:07:06 +0300

    Некий простенький тест))

--- Lesson_7/public/test.php
+++ Lesson_7/public/test.php
@@ -1,7 +1,1 @@
 <?php
-$x = 1;
-$y = 2;
-
-if (($x + $y) == 3)
-    echo "OK";
-else echo "Filed";
--- Lesson_7/tests/ProductTest.php
+++ Lesson_7/tests/ProductTest.php
@@ -4,19 +4,32 @@
 namespace app\tests;
 
 use app\models\entities\Product;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
-    public function testProduct() {
-        $name = "Чай";
-        $product = new Product($name, "Цейлонский", 55);
+    /**
+     * @dataProvider providerFactorial
+     * @param $name
+     * @param $description
+     * @param $price
+     * @throws Exception
+     */
+    public function testProduct($name, $description, $price) {
+        $product = new Product($name, $description, $price);
         $this->assertEquals($name, $product->name);
-        /*
-        if (strpos(Models::class, "app\\") === 0) {}
-        if (array_slice("\\", get_class(new Models()),1,1) === ["models"]) {}
-
-        */
+        $this->assertEquals($description, $product->description);
+        $this->assertEquals($price, $product->price);
     }
+    public function providerFactorial()
+    {
+        return array (
+            array (0, 1, "as"),
+            array ("asdfwer\\fawe", "asdfasd", 235),
+            array ("gfv", "gfv", null)
+        );
+    }
+
 
 }
